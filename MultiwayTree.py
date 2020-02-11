96f0fe64c083128256bc4c9703be46b1c7404421
dbasina <dbasina@asu.edu>
2020-02-10 22:09:30 -0700

    - Automated initialization of keyword nodes -Finished the Search algorithm - removed keywordNode dictionary. keyword nodes are a part of the nodeDictionary, and can be referenced using their names.

--- MultiwayTree.py
+++ MultiwayTree.py
@@ -3,6 +3,7 @@ from DummyNode import DummyNode
 import random
 from collections import deque
 import time
+from random import randrange
 
 # Test case methods for dummy nodes
 def generateDummyNodes(n):
@@ -13,79 +14,56 @@ def generateDummyNodes(n):
     return nodeList
 
 def assignDummyKeyWords(nodeList):
-    keywordList = ["Facebook","Reddit","Twitter"]
+    keywordList = ["Facebook","Reddit","Twitter","Instagram","Snapchat","TikTok"]
     for node in nodeList:
-        numberOfKeywords = random.randint(1,3);
+        numberOfKeywords = random.randint(1,len(keywordList))
         for i in range(numberOfKeywords):
             node.keywordList.append(random.choice(keywordList))
         node.keywordList= list(set(node.keywordList))
 
 def assignDummyReferences(nodeList):
     for node in nodeList:
-        numberOfReferences = random.randint(1,15);
+        numberOfReferences =  randrange(30)
         for i in range(numberOfReferences):
-            node.references.append(random.randint(1,100))
+            node.references.append(random.randint(1,1000000))
 
 
 class multiwayTree:
     def __init__ (self):
         self.root = ArticleNode(0)
-        self.Facebook = ArticleNode(-1)
-        self.Reddit = ArticleNode(-2)
-        self.Twitter = ArticleNode(-3)
+        self.root.name = "Root"
         self.nodeDictionary = {
-            -3: self.Twitter,
-            -2:self.Reddit,
-            -1:self.Facebook,
             0:self.root
         }
-
-        self.keywordDictionary = {
-            "Twitter" : -3,
-            "Reddit"  : -2,
-            "Facebook" : -1
-        }
-        # Initialize root node and its relationships ( this needs to be automated based on keywords)
-        self.root.name = "Root"
-        self.Facebook.name = "Facebook"
-        self.Reddit.name = "Reddit"
-        self.Twitter.name = "Twitter"
-
-        self.root.references = [self.Facebook.articleID,self.Reddit.articleID,self.Twitter.articleID]
-        self.root.successors = [self.Facebook,self.Reddit,self.Twitter]
-
-        self.Facebook.predecessors = self.root
-        self.Reddit.predecessors = self.root
-        self.Twitter.predecessors = self.root
-
-        # Tree Variables
-        self.numberOfSubtrees= len(self.root.successors)
-
-
+        self.numberOfSubtrees = 0
+        self.keywords = []
+        
+    #Store all the article nodes in node dictionary using Article Id's as keys
     def initializeNodeDictionary(self,nodeList):
         for node in nodeList:
             self.nodeDictionary[node.articleID] = node
-    
+            for keyword in node.keywordList:
+                if (keyword not in self.keywords):
+                    self.numberOfSubtrees = self.numberOfSubtrees + 1
+                    self.keywords.append(keyword)
+                    self.nodeDictionary[keyword] = ArticleNode(-self.numberOfSubtrees)
+                    self.nodeDictionary[keyword].name = keyword
+                    self.nodeDictionary[keyword].predecessors = self.root
+                    self.root.successors = self.nodeDictionary[keyword]            
     def assignSubTrees(self):
         for i in range(1,len(self.nodeDictionary)-self.numberOfSubtrees):
-            # access node from nodeDictionary
-            # Iterate over Keywords
             for keyword in self.nodeDictionary[i].keywordList:
-                keywordKey = self.keywordDictionary[keyword]
-                self.nodeDictionary[keywordKey].successors.append(self.nodeDictionary[i])
-                self.nodeDictionary[i].subTreePredecessors.append(self.nodeDictionary[keywordKey])
-    
+                self.nodeDictionary[keyword].successors.append(self.nodeDictionary[i])
+                self.nodeDictionary[i].subTreePredecessors.append(self.nodeDictionary[keyword]) 
     def establishNodeRelationships(self):
         for i in range(1,len(self.nodeDictionary)-self.numberOfSubtrees):
             for reference in self.nodeDictionary[i].references:
                 self.nodeDictionary[i].successors.append(self.nodeDictionary[reference])
                 self.nodeDictionary[reference].predecessors.append(self.nodeDictionary[i])
-
     def initialize(self,nodeList):
         self.initializeNodeDictionary(nodeList)
         self.assignSubTrees()
         self.establishNodeRelationships()
-
     def printRelations(self):
         print ("Successors: ")
         count = 1
@@ -103,15 +81,23 @@ class multiwayTree:
             count+=1
             for predecessor in tree.nodeDictionary[i].predecessors:
                 print(predecessor.articleID,end = ' ')
-            print()
-        
-    def keyWordSearch(self,keyword):
-        
-        keyWordPosition = self.keywordDictionary[keyword]
-        root  = self.nodeDictionary[keyWordPosition]
+            print()   
+    def keyWordSearch(self,searchList):
+        min = len(self.nodeDictionary)
+        searchTree = None
+
+        #find the smallest subTree
+        for keyword in searchList:
+            if(len(self.nodeDictionary[keyword].successors)<min):
+                min = len(self.nodeDictionary[keyword].successors)
+                searchTree = self.nodeDictionary[keyword]
+
+        #remove subtree from searchlist
+        searchList.remove(searchTree.name)  
         searchOutput = []
-        for successor in root.successors:
-            searchOutput.append(successor.articleID)
+        for successor in searchTree.successors:
+            if(set(searchList).issubset(set(successor.keywordList))):
+                searchOutput.append(successor.articleID)
         return searchOutput
             
 
@@ -131,10 +117,10 @@ tree.initialize(nodeList)
 print("Initializaiton time: %s seconds" % (time.time() - startTime))
 
 startTime = time.time()
-searchResults = tree.keyWordSearch("Facebook")
+searchResults = tree.keyWordSearch(["Facebook","Twitter"])
 print("Search time: %s seconds" % (time.time() - startTime))
 
-print("Number of Facebook articles: ",len(searchResults))
-
+print("Number of articles :", len(searchResults))
+print("Facebook: ",len(tree.nodeDictionary["Facebook"].successors),"Twitter: ",len(tree.nodeDictionary["Twitter"].successors))
 #Tested on dummy nodes only. Change nodeList variable to vary the dummy nodes used
 
---
+++ Random_Test.py
@@ -0,0 +1,6 @@
+random_dict ={
+    "a": 5,
+    -0: 2,
+    "abc":3
+}
+print(random_dict["a"])
