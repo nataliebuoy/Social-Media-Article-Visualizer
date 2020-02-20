c11cc36fa3806b78d95e2cdf469cd685fa370a70
Divesh Basina <AgNI@diveshs-air.3521.dhcp.asu.edu>
2020-02-20 13:13:41 -0700

    made changes to tester

--- Random_Test.py
+++ Random_Test.py
@@ -23,7 +23,7 @@ tree.initialize(nodeList)
 #Search parameters
 
 numberOfKeywords = random.randint(1,len(tree.keywords))
-numberOfTestSearches = 10000
+numberOfTestSearches = 100
 count = 0
 successfulSearches=[]
 for i in range(numberOfTestSearches):
@@ -50,3 +50,38 @@ for keyword in tree.keywords:
 print("Number of Categorized Articles = ",categorizedArticles)
 print("All keywords identified: \n", tree.keywords)
 print("Successful Searches: \n", successfulSearches)
+
+
+# import json
+# import os
+# import glob
+# import pprint
+# import pandas as pd
+
+# def intersection (list1,list2):
+#     return list(set(list1) & set(list2))
+# #Convert keywords in adhoc_wordlist.csv into lowercase list of keywords
+# keywords= pd.read_csv("adhoc_wordlist.csv")
+# df = pd.DataFrame(keywords)
+# keywordList = df[["KEYWORDS"]].values.tolist()
+# keywordList = [j[0] for j in keywordList]
+# keywordList = [x.lower() for x in keywordList]
+# keywordList = [x.strip() for x in keywordList]
+# print (keywordList)
+
+# # access abstracts of each of the downloaded .json files and check for keywords
+# path = '/Users/AgNI/Documents/Capstone/2018-10-25'
+# count = 0 
+# for filename in glob.glob(os.path.join(path, '*.json')): #only process .JSON files in folder.      
+#     with open(filename, encoding='utf-8', mode='r') as currentFile:
+#         data=currentFile.read().replace('\n', '')
+#         words = json.loads(data)["abstract"]
+#         words = words.split()
+#         words = [x.strip() for x in words]
+#         words = [x.lower() for x in words]
+#         words = list(set(words))
+#         match = len(intersection(words,keywordList))
+#         if match>1 :
+#             count+=1
+#         #print(list(intersection(words,keywordList)))
+# print ("Total Articles hit",count)
