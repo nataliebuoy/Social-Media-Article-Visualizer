8b32ecb2d4a1ec30f5fbdb39e89f8d42c1986dcc
kalexiev <45160957+kalexiev@users.noreply.github.com>
2020-04-11 17:32:27 -0700

    Add files via upload

---
+++ GetAuthors.py
@@ -0,0 +1,80 @@
+import csv
+
+class GetAuthors():
+    
+    def __init__(self):
+        print("")
+
+        self.authorDict = {}
+
+    def getAuthor(self):
+        #row = [article_ID, author_name]
+        with open('articleAuthor.csv', encoding="utf8") as csv_file:
+           
+            readArticles = csv.reader(csv_file, delimiter=',')
+            
+            # line_count = 0
+            for row in readArticles:
+                author = row[1].lower()
+                articles = row[0].lower()
+                  
+                if author not in self.authorDict.keys():
+                    update = [articles]
+                    self.authorDict.update({author: update})
+                else:
+                    update = self.authorDict.get(author)
+                    update.append(articles)
+                    self.authorDict.update({author: update})
+
+            # for author in self.authorDict.keys():
+            #     print("Author is: " + author)
+            #     print("Articles are: ")
+            #     print(self.authorDict[author])
+            #     print("")
+
+           # print(f'Processed {line_count} lines.')
+
+
+    # Returns article IDs of articles written by author (matches full author name or by last name)
+    def authorSearch(self, authorName, authorDict):
+
+        authorName = authorName.lower()
+        lastName = authorName.split(" ")[-1]  
+        firstName = authorName.split(" ")[0]
+        found = 0
+        
+        # Exact full name match
+        if authorName in self.authorDict:
+           print("Articles by: " + authorName)
+           print(authorDict.get(authorName))
+           found = 1
+        else:
+            for authorName in self.authorDict:    
+                # Middle initial ignored, first and last name match           
+                if authorName.endswith(lastName) and authorName.startswith(firstName):
+                    print("Articles by: " + authorName)
+                    print(authorDict.get(authorName))
+                    found = 1
+                # When only the last name is searched, return articles by authors with the last name
+                elif authorName.endswith(lastName) and firstName == lastName:
+                    print("Articles by name: " + authorName)
+                    print(authorDict.get(authorName))
+                    found = 1
+
+        if(found == 0):
+            print("No articles by this author!")
+
+    def getAuthors(self):
+        self.getAuthor()
+        return self.authorDict
+
+
+if __name__ == "__main__":
+
+    obj = GetAuthors()
+    authorDictionary = obj.getAuthors()
+
+    # authorName = "Ronald Burt"
+    # obj.authorSearch(authorName, authorDictionary)
+
+    #print("author count: " + str(len(authorDictionary)))
